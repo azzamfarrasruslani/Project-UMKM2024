d30c966cc6eab7f55508274681f8211f2bb3d056
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2024-11-30 16:16:49 +0700

    Test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravesdfsdfl</title>
+        <title>Laravesdfsdflsssssssss</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
